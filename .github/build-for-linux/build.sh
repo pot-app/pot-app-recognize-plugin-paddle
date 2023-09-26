c45416fca0b830ef25cc6384c18c7e07f5144d6c
Pylogmon <pylogmon@outlook.com>
2023-09-26 20:36:41 +0800

    feat: Add Paddle-json

--- .github/build-for-linux/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cargo build --target "$INPUT_TARGET" --release
-mv target/$INPUT_TARGET/release/libplugin.so ./plugin.so
--- src/lib.rs
+++ src/lib.rs
@@ -1,48 +1,58 @@
-use serde_json::Value;
+use dirs::{cache_dir, config_dir};
+use serde_json::{json, Value};
 use std::collections::HashMap;
 use std::error::Error;
+#[cfg(target_os = "windows")]
+use std::os::windows::process::CommandExt;
+use std::process::Command;
 
 #[no_mangle]
 pub fn recognize(
-    base64: &str, // 图像Base64
-    lang: &str,   // 识别语言
-    // (pot会根据info.json 中的 language 字段传入插件需要的语言代码，无需再次转换)
-    needs: HashMap<String, String>, // 插件需要的其他参数,由info.json定义
+    _base64: &str,
+    _lang: &str,
+    _needs: HashMap<String, String>,
 ) -> Result<Value, Box<dyn Error>> {
-    let client = reqwest::blocking::ClientBuilder::new().build()?;
+    let config_dir_path = config_dir().unwrap();
 
-    let apikey = match needs.get("apikey") {
-        Some(apikey) => apikey.to_string(),
-        None => return Err("apikey not found".into()),
-    };
+    let plugin_path = config_dir_path
+        .join("com.pot-app.desktop")
+        .join("plugins")
+        .join("recognize")
+        .join("[plugin].com.pot-app.paddle");
+    let paddle_dir_path = plugin_path.join("PaddleOCR-json_v.1.3.0");
+    let paddle_exe_path = paddle_dir_path.join("PaddleOCR-json.exe");
 
-    let base64 = format!("data:image/png;base64,{}", base64);
-    let mut form_data = HashMap::new();
-    form_data.insert("base64Image", base64);
-    form_data.insert("OCREngine", "2".to_string());
+    let cache_dir_path = cache_dir().unwrap();
+    let image_path = cache_dir_path
+        .join("com.pot-app.desktop")
+        .join("pot_screenshot_cut.png");
 
-    let res: Value = client
-        .post("https://api.ocr.space/parse/image")
-        .header("apikey", apikey)
-        .header("content-type", "application/x-www-form-urlencoded")
-        .form(&form_data)
-        .send()?
-        .json()?;
+    let mut cmd = Command::new("cmd");
+    let cmd = cmd.creation_flags(0x08000000);
+    let cmd = cmd.args(["/c", &paddle_exe_path.to_str().unwrap()]);
 
-    fn parse_result(res: Value) -> Option<String> {
-        let result_list = res.as_object()?.get("ParsedResults")?.as_array()?;
-        let mut result = String::new();
-        for i in result_list {
-            let parsed_text = i.as_object()?.get("ParsedText")?.as_str()?;
-            result.push_str(parsed_text);
+    let output = cmd
+        .current_dir(paddle_dir_path)
+        .arg(&format!("--image_path={}", image_path.to_str().unwrap()))
+        .output()?;
+
+    let result = String::from_utf8_lossy(&output.stdout);
+    let result = result.split("OCR init completed.\r\n").last().unwrap();
+    let json = serde_json::from_str(result.trim())?;
+
+    fn parse_json(res: Value) -> Option<String> {
+        let mut target = String::new();
+        let data = res.as_object()?.get("data")?.as_array()?;
+        for line in data {
+            let str = line.as_object()?.get("text")?.as_str()?;
+            target.push_str(str);
+            target.push_str("\n");
         }
-        Some(result)
+        Some(target)
     }
-
-    if let Some(result) = parse_result(res) {
-        return Ok(Value::String(result));
-    } else {
-        return Err("Response Parse Error".into());
+    match parse_json(json) {
+        Some(text) => Ok(Value::String(text)),
+        None => Err("Parse Error".into()),
     }
 }
 
@@ -51,9 +61,8 @@ mod tests {
     use super::*;
     #[test]
     fn try_request() {
-        let mut needs = HashMap::new();
-        needs.insert("apikey".to_string(), "".to_string());
-        let result = recognize("iVBORw0KGgoAAAANSUhEUgAAADsAAAAeCAYAAACSRGY2AAAAAXNSR0IArs4c6QAAArNJREFUWEftl19IU1Ecxz+O5uQiNTCJkNj0ZWhkSOyh7CEy0CWZQQoTWYgvk17KFAdr9GBBYGb/qD0oUpgSCZViGkTRQ/hwEVOYIIhlMF8kUjbGZGPFdGtrGvcWzTa79/Gec+79fb7fc36/38nQ6/Xf+E+eDAV2mzqdns6WtDNRqYP5UQ71D8i2RoGVLdW/mqg4K6287G3sqHtEdYEP8clrdpZXYdCCxzWE/dkHjp5poXa/AMEVZodvU+ea2/Dn0n2NnK8wYsgVQAWEAng+TfHiZTddy75NI83LtdBRfSS2xruIONKNNftccs9sFPbLkpqcXUCmei1At2uO3YU6CKnR7AhDLDJ204bdH4u/tKSdjkodmvCrEKz6A2iE9fWEVhAftmF1JwBnmxm0msjPinzHH2A1U42GFcSJZYzGJCaodVhYnRqgZngUCmw8rStC419gzOnA7iuio8HG8b3wccTC2clIkFkWhppPkKcK4H7bTev7cWbDQ5kHcZxqorpQAO8M929dp+eHPgJtNXepNajh6wx9j+9E3BeoONBCc7mOnCx18rJxFDYGYmbwson85Sm67nXSB9SXO7loFPCIDzj2anwtdOPhTpxlueB+h7W3BzF+w6pM9F8wYxACTPc30jAfHTTR22ymeMP78HicEMkqPX8Ku5kAMV6Ba/VOKvQJu4GIkCzx5sYlWuOOxE8CphcsbBQxjBOFXeD5VQftiekr2aUnOc4qsNvV2W12ZuVlYx9irxWrO82zMXLqbFz5WseVqLNlOnKyU7DOhkP/qx2Uysf05BLFJVvQQf1uUxHdmIY9Fq5UxfW5wQCezxK9sbYKx+mTGPMi/fRW9cbSd4rUnyH71pP6KNIRKrDSGqXnDMXZ9PRNOmrF2USNtFotXq+XYDAoLV8Kz5DlrAKbwg7+KrTvuhRWXxXeDuUAAAAASUVORK5CYII=", "", needs).unwrap();
-        println!("{result}");
+        let needs = HashMap::new();
+        let result = recognize("", "", needs);
+        println!("{result:?}");
     }
 }
